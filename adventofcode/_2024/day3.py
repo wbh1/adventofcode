c777dca65617f0143fad1c5863f1a734ce78d47b
Will Hegedus <will@wbhegedus.me>
2024-12-03 07:06:38 -0500

    ref(day3): simplify/make DRY

--- adventofcode/_2024/day3.py
+++ adventofcode/_2024/day3.py
@@ -5,23 +5,22 @@ from adventofcode.utils.Puzzle import Puzzle
 
 class Day3(Puzzle):
     day = 3
+    pattern = re.compile(
+        r"(?P<inst>do\(\)|don't\(\))|mul\((?P<num1>\d{,3}),(?P<num2>\d{,3})\)"
+    )
 
     def part1(self) -> int:
-        pattern = re.compile(r"mul\((?P<num1>\d{,3}),(?P<num2>\d{,3})\)")
-        total = 0
-        for line in self.data:
-            for match in pattern.finditer(line):
-                total += int(match.group("num1")) * int(match.group("num2"))
-        return total
+        return sum(
+            int(match.group("num1") or 0) * int(match.group("num2") or 0)
+            for line in self.data
+            for match in self.pattern.finditer(line)
+        )
 
     def part2(self) -> int:
-        pattern = re.compile(
-            r"(?P<inst>do\(\)|don't\(\))|mul\((?P<num1>\d{,3}),(?P<num2>\d{,3})\)"
-        )
         total = 0
         mul_enabled = True
         for line in self.data:
-            for match in pattern.finditer(line):
+            for match in self.pattern.finditer(line):
                 if inst := match.group("inst"):
                     if inst == "don't()":
                         mul_enabled = False
