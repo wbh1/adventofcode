b0ad211d953f73f8cfe72d56927ee4213ba2f273
Will Hegedus <whegedus@akamai.com>
2025-12-03 17:47:25 -0500

    ref(day3): simplify/unify solution for parts 1 and 2

--- tests/2025/test_day3.py
+++ tests/2025/test_day3.py
@@ -11,11 +11,11 @@ d.debug = True
 
 @pytest.mark.parametrize(["bank", "joltage"], [(data[0], 98), (data[1], 89), (data[2], 78), (data[3], 92)])
 def test_joltage_part1(bank, joltage):
-    assert d.max_joltage_part1(bank) == joltage
+    assert d.max_joltage(bank) == joltage
 
 @pytest.mark.parametrize(["bank", "joltage"], [(data[0], 987654321111), (data[1], 811111111119), (data[2], 434234234278), (data[3], 888911112111)])
 def test_joltage_part2(bank, joltage):
-    assert d.max_joltage_part2(bank) == joltage
+    assert d.max_joltage(bank, batteries=12) == joltage
 
 def test_part1():
     assert d.part1() == 357
