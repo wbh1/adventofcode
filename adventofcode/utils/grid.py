d99f6d63ba4009815216dd194d751dce7cf0ac40
Will Hegedus <will@wbhegedus.me>
2025-12-04 06:59:53 -0500

    chore: fmt and document Grid utility class

--- adventofcode/utils/grid.py
+++ adventofcode/utils/grid.py
@@ -14,26 +14,21 @@ UP, DOWN, LEFT, RIGHT = (-1, 0), (1, 0), (0, -1), (0, 1)
 def deltas_2d() -> Iterable[Tuple[int, int]]:
   return zip(DR, DC)
 
+
 class Grid:
   """Creates a dictionary of coordinates to value.
     e.g. {(0,0): "@", (0,1): ".", (1,1): "@"}
 
   NOTE: the coordinates are in (y,x) form and NOT (x,y) form
   """
+
   def __init__(self, grid: List[str]) -> None:
     self.grid = defaultdict(
-        lambda: "",
-        {
-            (y, x): grid[y][x]
-            for y in range(len(grid))
-            for x in range(len(grid[0]))
-        },
+      lambda: "",
+      {(y, x): grid[y][x] for y in range(len(grid)) for x in range(len(grid[0]))},
     )
 
   def neighbors(self, coordinate: Tuple[int, int]) -> List[Any]:
+    """Returns a list of values for each of the 8 possible neighbors of the given coordinate"""
     y, x = coordinate
-    return [
-        self.grid.get((y + delta_y, x + delta_x))
-        for delta_x, delta_y in deltas_2d()
-    ]
-
+    return [self.grid.get((y + delta_y, x + delta_x)) for delta_x, delta_y in deltas_2d()]
