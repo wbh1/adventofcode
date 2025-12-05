324f3794b0fa6c283bcb3a45c1b3608d7688fcdf
Will Hegedus <whegedus@akamai.com>
2025-12-05 12:37:47 -0500

    ref(day5): refine solution

--- adventofcode/_2025/day5.py
+++ adventofcode/_2025/day5.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from adventofcode.utils.Puzzle import Puzzle
 
 
@@ -20,8 +20,8 @@ class Day5(Puzzle):
     for i in self.ingredients:
       fresh = False
       for r in self.ingredient_ranges:
-        low, high = r.split("-", 1)
-        fresh = int(low) <= int(i) <= int(high)
+        low, high = map(int, r.split("-", 1))
+        fresh = low <= int(i) <= high
         if fresh:
           break
 
@@ -32,50 +32,22 @@ class Day5(Puzzle):
 
   def flatten_ranges(self):
     """Combine ranges to the maximum possible extent"""
-    combined_ranges = self._flatten_ranges()
-    while combined_ranges != 0:
-      combined_ranges = self._flatten_ranges()
+    self.ingredient_ranges = sorted(self.ingredient_ranges, key=lambda x: int(x.split("-")[0]))
+    merged: List[Tuple[int, int]] = []
+    for r in self.ingredient_ranges:
+      s, e = map(int, r.split("-"))
+      if not merged or s > merged[-1][1]:
+        merged.append((s, e))
+      else:
+        # Overlap or touch: extend the current range
+        merged[-1] = (merged[-1][0], max(merged[-1][1], e))
 
-  def _flatten_ranges(self) -> int:
-    """Combine ranges where possible and return number of ranges that were altered"""
-    new_ranges: List[str] = []
-    combined_ct = 0
-    self.log("Starting a flattening...")
-    for i1, r1 in enumerate(self.ingredient_ranges):
-      if r1 == "":
-        continue
-      r1_start, r1_end = r1.split("-", 1)
-      combined = False
-      for i2, r2 in enumerate(self.ingredient_ranges):
-        if i1 == i2:
-          continue
-        if r2 == "":
-          continue
-        r2_start, r2_end = r2.split("-", 1)
-
-        if (
-          (int(r2_start) <= int(r1_end) or r1_start == r2_start) and int(r2_end) >= int(r1_end)
-        ) or r1 == r2:
-          new_ranges.append(f"{r1_start}-{r2_end}")
-          self.log(f"  Combined '{r1}' and '{r2}' into '{r1_start}-{r2_end}'")
-          self.ingredient_ranges[i2] = ""
-          combined = True
-          combined_ct += 1
-          break
-      if not combined:
-        new_ranges.append(r1)
-    self.ingredient_ranges = sorted(new_ranges, key=lambda x: int(x.split("-")[0]))
-    return combined_ct
+    self.ingredient_ranges = [f"{s}-{e}" for s, e in merged]
 
   def part2(self) -> int:
-    self.ingredient_ranges = sorted(self.ingredient_ranges, key=lambda x: int(x.split("-")[0]))
     self.flatten_ranges()
-    for r in self.ingredient_ranges:
-      self.log(f"{r}")
-
     fresh_ids = 0
     for r in self.ingredient_ranges:
-      low, high = r.split("-", 1)
-      fresh_ids += int(high) - int(low) + 1
-
+      low, high = map(int, r.split("-", 1))
+      fresh_ids += high - low + 1
     return fresh_ids
