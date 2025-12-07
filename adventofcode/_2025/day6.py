15f147155aad5e2b8210c7a3df579222e0b89163
Will Hegedus <will@wbhegedus.me>
2025-12-06 22:13:30 -0500

    ref(day6): clean up my solution for day6

--- adventofcode/_2025/day6.py
+++ adventofcode/_2025/day6.py
@@ -28,44 +28,37 @@ class Day6(Puzzle):
     self.parsed_data = parse_fixed_columns(self.data)
     self.operations = [op for op in self.data[-1].split() if op]
 
+  def do_homework(self, op: str, inputs: List[int]) -> int:
+    match op:
+      case "*":
+        return math.prod(inputs)
+      case "+":
+        return sum(inputs)
+      case _:
+        raise ValueError(f"Unknown math operation to perform: `{op}`")
+
   def part1(self) -> int:
-    self.log(f"Parsed data: {self.parsed_data}")
-    self.log(f"Operations: {self.operations}")
     results: List[int] = []
     for i, op in enumerate(self.operations):
       inputs = [int(row[i]) for row in self.parsed_data]
-      self.log(f"str Inputs: {[row[i] for row in self.parsed_data]}")
-      self.log(f"Inputs: {inputs}")
-      match op:
-        case "*":
-          results.append(math.prod(inputs))
-        case "+":
-          results.append(sum(inputs))
-        case _:
-          raise ValueError(f"Unknown math operation to perform: `{op}`")
+      results.append(self.do_homework(op, inputs))
 
     return sum(results)
 
   def part2(self) -> int:
     results: List[int] = []
+    # reverse the operations
     for i, op in enumerate(reversed(self.operations)):
+      # reverse the ordering of columns (retrieve from the end first)
       nums = [row[len(row) - 1 - i] for row in self.parsed_data]
-      self.log(f"Nums: {nums}")
       inputs = []
       for c in range(len(nums[0]), 0, -1):
-        self.log(f"   nums={nums} c={c}")
+        # Maybe by luck or intention, there are no 0's in my input
+        # so I can do this hack in case all rows for a column are empty space
         number = int("".join([n[c - 1][::-1] for n in nums]).strip() or 0)
         if number:
           inputs.append(number)
 
-      self.log(f"Inputs: {inputs}; op=`{op}`")
-      match op:
-        case "*":
-          results.append(math.prod(inputs))
-        case "+":
-          results.append(sum(inputs))
-        case _:
-          raise ValueError(f"Unknown math operation to perform: `{op}`")
+      results.append(self.do_homework(op, inputs))
 
     return sum(results)
-
