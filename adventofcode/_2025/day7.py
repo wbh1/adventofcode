9f98e1df8edf182498f463ae6b5598cc70b412a4
Will Hegedus <whegedus@akamai.com>
2025-12-08 09:23:20 -0500

    ref(day7): optimize solution to prevent duplicate work

--- adventofcode/_2025/day7.py
+++ adventofcode/_2025/day7.py
@@ -1,32 +1,37 @@
-from typing import Set
+from typing import Dict
 from adventofcode.utils.Puzzle import Puzzle
-from collections import Counter
 
 
 class Day7(Puzzle):
   day = 7
+  beams: Dict[int, int]
+  splits: int
 
-  def count_splits(self, part2=False) -> int:
-    active_beam_columns: Set[int] = set([self.data[0].index("S")])
-    tachyons = Counter(active_beam_columns)
+  def traverse_manifold(self):
+    if hasattr(self, "splits") and hasattr(self, "beams"):
+      return
+    start_col = self.data[0].index("S")
+    beams = {start_col: 1}
     splits = 0
     for row in self.data[1:]:
-      for col, value in enumerate(row):
-        if col in active_beam_columns and value == "^":
-          active_beam_columns.remove(col)
-          active_beam_columns.update([col - 1, col + 1])
-          tachyons[col - 1] += tachyons[col]
-          tachyons[col + 1] += tachyons[col]
-          del tachyons[col]
+      next_beams = {}
+      for col, count in beams.items():
+        if row[col] == "^":
+          next_beams[col - 1] = next_beams.get(col - 1, 0) + count
+          next_beams[col + 1] = next_beams.get(col + 1, 0) + count
           splits += 1
-    if part2:
-      return sum(tachyons.values())
-    else:
-      return splits
+        else:
+          # No split: beam continues straight
+          next_beams[col] = next_beams.get(col, 0) + count
+      beams = next_beams
+    self.beams = beams
+    self.splits = splits
 
   def part1(self) -> int:
-    return self.count_splits()
+    self.traverse_manifold()
+    return self.splits
 
   def part2(self) -> int:
-    return self.count_splits(part2=True)
+    self.traverse_manifold()
+    return sum(self.beams.values())  # total paths (tachyon count)
 
